c348b79862d283d437df1e9914e075c3a44d0620
Kelvin <kelvinmarquesk@gmail.com>
2023-04-26 11:23:07 -0300

    Adicionado o método Delete de autor por id e o Post para adicionar autores

--- api01/index.js
+++ api01/index.js
@@ -64,3 +64,43 @@ app.get('/autor/:id', (req, res) => {
     }
   });
 });
+
+app.delete('/autor/:id', (req, res) => {
+  const idAutor = req.params.id;
+  const sql = 'DELETE FROM tbAutor WHERE IdAutor = ?';
+  con.query(sql, [idAutor], (erroComandoSQL, result, fields) => {
+    if (erroComandoSQL) {
+      throw erroComandoSQL;
+    }
+    
+    if (result.affectedRows > 0) {
+      res.status(200).send('Registro excluído com sucesso');
+    }
+    else {
+      res.status(404).send('Não encontrado');
+    }
+  });
+});
+
+app.post('/autor', (req, res) => {
+  const noautor = req.body.noautor;
+  const idnacionalidade = req.body.idnacionalidade;
+
+  const sql = 'INSERT INTO tbAutor (NoAutor, IdNacionalidade) VALUES (?, ?)';
+  con.query(sql, [noautor, idnacionalidade], (erroComandoSQL, result, fields) => {
+    if (erroComandoSQL) {
+      throw erroComandoSQL;
+    }
+    
+    if (result.affectedRows > 0) {
+      res.status(200).send('Registro incluído com sucesso');
+    }
+    else {
+      res.status(400).send('Erro ao incluir o registro');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
