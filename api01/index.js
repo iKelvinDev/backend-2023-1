0d28a59cd0b95476493f2d0a58a3a5585fd62bdf
Kelvin <kelvinmarquesk@gmail.com>
2023-04-26 11:58:47 -0300

    Adicionado os métodos Get, Delete, Post e Put para Editora

--- api01/index.js
+++ api01/index.js
@@ -121,6 +121,70 @@ app.put('/autor/:id', (req, res) => {
   });
 });
 
+/* Editora */
+
+app.get('/editora', (req, res) => {
+  con.query('SELECT * FROM tbEditora', (erroComandoSQL, result, fields) => {
+    if (erroComandoSQL) {
+      throw erroComandoSQL;
+    }
+    res.status(200).send(result);
+  });
+});
+
+app.get('/editora/:id', (req, res) => {
+  const idEditora = req.params.id;
+  const sql = 'SELECT * FROM tbEditora WHERE IdEditora = ?';
+  con.query(sql, [idEditora], (erroComandoSQL, result, fields) => {
+    if (erroComandoSQL) {
+      throw erroComandoSQL;
+    }
+    
+    if (result.length > 0) {
+      res.status(200).send(result);
+    }
+    else {
+      res.status(404).send('Não encontrado');
+    }
+  });
+});
+
+app.delete('/editora/:id', (req, res) => {
+  const idEditora = req.params.id;
+  const sql = 'DELETE FROM tbEditora WHERE IdEditora = ?';
+  con.query(sql, [idEditora], (erroComandoSQL, result, fields) => {
+    if (erroComandoSQL) {
+      throw erroComandoSQL;
+    }
+    
+    if (result.affectedRows > 0) {
+      res.status(200).send('Registro excluído com sucesso');
+    }
+    else {
+      res.status(404).send('Não encontrado');
+    }
+  });
+});
+
+app.post('/editora', (req, res) => {
+  const noeditora = req.body.noeditora;
+  const ideditora = req.body.ideditora;
+
+  const sql = 'INSERT INTO tbEditora (NoEditora, IdEditora) VALUES (?, ?)';
+  con.query(sql, [noeditora, ideditora], (erroComandoSQL, result, fields) => {
+    if (erroComandoSQL) {
+      throw erroComandoSQL;
+    }
+    
+    if (result.affectedRows > 0) {
+      res.status(200).send('Registro incluído com sucesso');
+    }
+    else {
+      res.status(400).send('Erro ao incluir o registro');
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
